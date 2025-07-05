39b59054f9d277463a1881228966c8d72228e5b9
Saurav <sauravsaurav0@gmail.com>
2025-07-05 11:27:09 +0000

    Add function to listen for all incoming requests on the port

--- startingServer/index.js
+++ startingServer/index.js
@@ -1,3 +1,12 @@
+// Importing express after installing it using npm
 import express from 'express';
+// creating an instance of express
 const app = express();
+// specify the port on which the server would be listening to..
 const port = 8545;
+
+// this is telling express that you have to listen to the port and do what the callback is asking you to
+app.listen(port, ()=>{
+
+    console.log(`Listening on the port ${port}`);
+})

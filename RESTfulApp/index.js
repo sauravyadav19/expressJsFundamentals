33129cbdac329b3eeaff0f840153f95be29f14f0
Saurav <sauravsaurav0@gmail.com>
2025-07-27 04:31:12 +0000

    Add the capability to edit a blog

--- RESTfulApp/index.js
+++ RESTfulApp/index.js
@@ -2,9 +2,11 @@
 // Importing our required Modules:
 // Express => is our backend framework.
 // path => for using an absolute path for configuration rather than relative.
+// method-override => help to deal with request other than GET or POST from a form.
 const express = require('express');
 const path = require('path');
 let { blogs, uuid } = require('./data');
+const methodOverride = require('method-override');
 //-----------------------------------------------------------------------------------------------------
 //Creating an Instance our Express app:
 const app = express();
@@ -22,6 +24,15 @@ app.use(express.static(path.join(__dirname,'public')));
 // we need to tell express so it can decoded it 
 // this is also true for JSON file sent for that we need to use  'app.use(express.json())'
 app.use(express.urlencoded())
+// when we have a form in html we can only specify the METHOD to be either GET or POST
+// but if we want to use PATCH, PUT or DELETE, we have to trick the form in sending that 
+// kind of the request, for theat we have library call methodOverride, we allow us to do somethign like this
+// <form action = '/blog/<%=data.id%>?_method=PATCH' method="POST" >
+// here even thought the METHOD is set to POST we have created a hidden value in the action _method="PATCH"
+// and since every incoming request has to go through the middleware, this middleware where we using methodOverrid
+// will trick our server into thinking that we are getting a PATCH request even though it is a POST request 
+// that was originally made.
+app.use(methodOverride('_method'));
 //-----------------------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------------------
 //Route Handlers
@@ -47,8 +58,29 @@ app.get('/blog/:id',(request,response)=>{
             response.render('expandedPage.ejs',{data:blog});
         }
     }
-    
 
+})
+app.get('/blog/:id/edit',(request,response)=>{
+    const id = (request.params.id).trim();
+    for(let blog of blogs){
+        if(blog.id === id){
+            response.render('editBlog.ejs',{data:blog});
+            break;
+        }
+    }
+})
+
+app.patch('/blog/:id',(request,response)=>{
+    const id = (request.params.id).trim();
+    const {title, author,article } = request.body;
+    for(let blog of blogs){
+        if(blog.id === id){
+            blog.title = title;
+            blog.author = author;
+            blog.content = article;
+            response.redirect('/');
+        }
+    }
 })
 app.listen(port,()=>{
     console.log("LISTENING ON PORT 8545...");

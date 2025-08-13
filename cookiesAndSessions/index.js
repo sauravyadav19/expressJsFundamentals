1bebe8894ea5d32ec9d703a8483b755adb1253f6
Saurav <sauravsaurav0@gmail.com>
2025-08-13 02:03:02 +0000

    Send a Cookie

--- cookiesAndSessions/index.js
+++ cookiesAndSessions/index.js
@@ -3,6 +3,11 @@ const app = express();
 
 
 app.get('/',(request,response)=>{
+    // This is how we send cookie 
+    // specifiy the the key as the first argument and value as the second argument
+    // optionally you can pass the third argument which specifies other properties 
+    // releated to this cookie, (like expiry,httpOnly... and others).
+    response.cookie('Name','Saurav');
     response.send('This is your Home page')
 })
 
